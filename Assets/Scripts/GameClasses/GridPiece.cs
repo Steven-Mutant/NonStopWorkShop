bfa23c17e608f53d001090d6a08f781111febb10
steven-mutant <vaccari.steven@gmail.com>
2016-09-20 09:45:43 -0700

    enums

--- Assets/Scripts/GameClasses/GridPiece.cs
+++ Assets/Scripts/GameClasses/GridPiece.cs
@@ -1,20 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+public enum IOStreamDir{
+	Null,
+	Top,
+	Bottom,
+	Right,
+	Left,
+	TopRight,
+	TopLeft,
+	BottomRight,
+	BottomLeft,
+	LeftTop,
+	LeftBottom,
+	RightTop,
+	RightBottom
+}
+		
+
 public class GridPiece : MonoBehaviour {
-
+	
 	Grid myGrid;
-
 	public Color myColor;
 
+	private IOStreamDir InputOne;
+	private IOStreamDir OutputOne;
+
+	private IOStreamDir InputTwo;
+	private IOStreamDir OutputTwo;
 
 
+	public void SetStreamIO(IOStreamDir Ione, IOStreamDir Oone, IOStreamDir Itwo = IOStreamDir.Null, IOStreamDir Otwo = IOStreamDir.Null){
+		InputOne = Ione;
+		OutputOne = Oone;
+
+		InputTwo = Itwo;
+		OutputTwo = Otwo;
+
+	}
 
 	void OnDrawGizmosSelected() {
-
-
-
-
+		
 		Gizmos.color = myColor;
 		Gizmos.DrawCube(transform.position, new Vector3(1, 1, 1));
 	}
